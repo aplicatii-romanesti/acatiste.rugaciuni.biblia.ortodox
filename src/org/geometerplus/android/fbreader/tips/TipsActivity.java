d0505c7b07b8e08e3e85d3a223e935623abebca5
aplicatii romanesti <aplicatii.romanesti@gmail.com>
2012-01-30 17:48:01 +0200

    disable tips

--- src/org/geometerplus/android/fbreader/tips/TipsActivity.java
+++ src/org/geometerplus/android/fbreader/tips/TipsActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2011 Geometer Plus <contact@geometerplus.com>
+ * Copyright (C) 2009-2015 FBReader.ORG Limited <contact@fbreader.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,53 +29,104 @@ import org.geometerplus.zlibrary.core.resources.ZLResource;
 
 import org.geometerplus.zlibrary.ui.android.aplicatii.romanesti.R;
 
+import org.geometerplus.fbreader.Paths;
 import org.geometerplus.fbreader.tips.*;
 
 public class TipsActivity extends Activity {
-	private final TipsManager myManager = TipsManager.Instance();
+	public static final String INITIALIZE_ACTION = "android.fbreader.action.tips.INITIALIZE";
+	public static final String SHOW_TIP_ACTION = "android.fbreader.action.tips.SHOW_TIP";
+
+	private TipsManager myManager;
 
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 
+ 		myManager = new TipsManager(Paths.systemInfo(this));
+
+		final boolean doInitialize = INITIALIZE_ACTION.equals(getIntent().getAction());
+
 		setContentView(R.layout.tip);
 		final ZLResource dialogResource = ZLResource.resource("dialog");
 		final ZLResource resource = dialogResource.getResource("tips");
+		final ZLResource buttonResource = dialogResource.getResource("button");
 		final CheckBox checkBox = (CheckBox)findViewById(R.id.tip_checkbox);
-		checkBox.setText(resource.getResource("dontShowAgain").getValue());
 
-		final Button okButton =
-			(Button)findViewById(R.id.tip_buttons).findViewById(R.id.ok_button);
-		okButton.setText(dialogResource.getResource("button").getResource("ok").getValue());
-		okButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				myManager.ShowTipsOption.setValue(!checkBox.isChecked());
-				finish();
-			}
-		});
+		setTitle(resource.getResource("title").getValue());
 
-		final Button nextTipButton =
-			(Button)findViewById(R.id.tip_buttons).findViewById(R.id.cancel_button);
-		nextTipButton.setText(resource.getResource("more").getValue());
-		nextTipButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				showTip(nextTipButton);
-			}
-		});
+		if (doInitialize) {
+			myManager.TipsAreInitializedOption.setValue(true);//maryhit
+			myManager.ShowTipsOption.setValue(false);//maryhit
+			this.finish();//maryhit
+			/*
+			checkBox.setVisibility(View.GONE);
 
-		showTip(nextTipButton);
+			showText(resource.getResource("initializationText").getValue());
+
+			final Button yesButton =
+				(Button)findViewById(R.id.tip_buttons).findViewById(R.id.ok_button);
+			yesButton.setText(buttonResource.getResource("yes").getValue());
+			yesButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					myManager.TipsAreInitializedOption.setValue(true);
+					myManager.ShowTipsOption.setValue(true);
+					myManager.startDownloading();
+					finish();
+				}
+			});
+
+			final Button noButton =
+				(Button)findViewById(R.id.tip_buttons).findViewById(R.id.cancel_button);
+			noButton.setText(buttonResource.getResource("no").getValue());
+			noButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					myManager.TipsAreInitializedOption.setValue(true);
+					myManager.ShowTipsOption.setValue(false);
+					finish();
+				}
+			});*/ //maryhit
+		} else {
+			checkBox.setText(resource.getResource("dontShowAgain").getValue());
+
+			final Button okButton =
+				(Button)findViewById(R.id.tip_buttons).findViewById(R.id.ok_button);
+			okButton.setText(buttonResource.getResource("ok").getValue());
+			okButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					myManager.ShowTipsOption.setValue(!checkBox.isChecked());
+					finish();
+				}
+			});
+
+			final Button nextTipButton =
+				(Button)findViewById(R.id.tip_buttons).findViewById(R.id.cancel_button);
+			nextTipButton.setText(resource.getResource("more").getValue());
+			nextTipButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					showTip(nextTipButton);
+				}
+			});
+
+			showTip(nextTipButton);
+		}
 	}
 
 	private void showTip(Button nextTipButton) {
 		final Tip tip = myManager.getNextTip();
 		if (tip != null) {
 			setTitle(tip.Title);
-			final TextView textView = ((TextView)findViewById(R.id.tip_text));
-			textView.setText(tip.Content);
-			textView.setMovementMethod(LinkMovementMethod.getInstance());
+			showText(tip.Content);
 		}
 		nextTipButton.setEnabled(myManager.hasNextTip());
+	}
+
+	private void showText(CharSequence text) {
+		final TextView textView = (TextView)findViewById(R.id.tip_text);
+		textView.setText(text);
+		textView.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 }
